7cfe7c102d165e0eec8720208ddcdd1a440c5637
Oscar Herrera <mao.oscar@gmail.com>
2022-05-28 09:37:06 -0500

    Proyecto limpio

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ echo "$Red ADVERTENCIA: ESTE ESCRIPT ESTA DISEÑADO PARA IMPLEMENTAR EL SISTEMA 
 echo "$Red PROVISTO POR LA PND Y NO DEBERÁ USARSE PARA MODIFICAR UN SISTEMA EN PRODUCCIÓN, USE BAJO SU PROPIO RIESGO"
 echo "$Red DATAISMO SOFTWARE NO ES, NI SERÁ RESPONSABLE POR LA PERDIDA DE INFORMACIÓN O CUALQUIER OTRO DAÑO ATRIBUIBLE AL USO "
 echo "$Red DE ESTE SCRIPT. LEA LA DOCUMENTACIÓN ANTES DE EMPEZAR"
+echo "$Red COMPATIBLE POR EL MOMENTO CON UBUNTU 20.04 FOCAL"
 
 printf "\n"
 printf "\n"
@@ -36,7 +37,33 @@ git clone https://github.com/PDNMX/SistemaDeclaraciones_reportes.git
 
 #Instalamos dependencias:
 sudo apt update
-sudo apt install --yes php-cli php-dev php-mongodb docker docker-compose mongodb
+# Instalación de MONGO:
+
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# Install DOcker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Instalamos PHP
+sudo apt install --yes php-cli php-dev php-mongodb
 sudo pecl install mongodb
 # Instalamos composer:
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
