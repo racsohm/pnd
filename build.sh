fec8bb1127d0ce6e4d367905a6a2122889d6df6c
usuario <mao.oscar@gmail.com>
2022-05-29 00:53:30 -0500

    Correcciónes para iniciar mongo correctamente trans su instalación.

--- build.sh
+++ build.sh
@@ -64,7 +64,13 @@ sudo apt-get install --yes docker-ce docker-ce-cli containerd.io docker-compose-
 
 # Instalamos PHP
 sudo apt install --yes php-cli php-dev php-mongodb
+# INSTALAMOS MONGO:
 sudo pecl install mongodb
+sudo systemctl enable mongod
+sudo systemctl stop mongod
+sudo systemctl restart mongod
+
+
 # Instalamos composer:
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
